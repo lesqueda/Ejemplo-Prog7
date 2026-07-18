fefe7c4d6fb5e9bbc06220ad6b0b5d245542650c
Aldair-Gallardo <aldairgallardo0903@gmail.com>
2026-07-17 21:57:05 -0500

    Creacion de funciones multiplicar y dividir

--- operaciones.py
+++ operaciones.py
@@ -1,5 +1,21 @@
 import math
 
+def multiplicacion(a,b):
+    resultado = a * b
+    return resultado
+
+def division (a,b):
+    resultado = a /b
+    return resultado
+
+def multiplicacion(a,b):
+    resultado = a * b
+    return resultado
+
+def division (a,b):
+    resultado = a /b
+    return resultado
+
 def potencia(base, exponente):
     """
     Retorna el resultado de elevar un número (base) a una potencia (exponente).
@@ -15,3 +31,4 @@ def raiz_cuadrada(n):
         raise ValueError("El número no puede ser negativo para calcular la raíz cuadrada.")
     
     return math.sqrt(n)
+
